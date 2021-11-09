175e58bfcaea6917fa062d295fb710b27e3ae8f0
Elias-av <84996781+Elias-av@users.noreply.github.com>
2021-11-09 14:56:29 -0500

    Add files via upload

---
+++ Usuario.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+#nullable disable
+
+namespace ProyectoUSMP_GYM.Models.ModelDB
+{
+    public partial class Usuario
+    {
+        public Usuario()
+        {
+            Usuariologins = new HashSet<Usuariologin>();
+            Usuariomembresia = new HashSet<Usuariomembresium>();
+        }
+
+        public int PkUsuario { get; set; }
+        public string Dni { get; set; }
+        public string Nombre { get; set; }
+        public string Apellidopaterno { get; set; }
+        public string Apellidomaterno { get; set; }
+        public string Telefono { get; set; }
+        public string Direccion { get; set; }
+        public string Email { get; set; }
+        public int? Tipousuario { get; set; }
+        public int? Userweb { get; set; }
+        public int? FkPersonalcrea { get; set; }
+        public DateTime? Fechacrea { get; set; }
+        public int? FkPersonaledita { get; set; }
+        public DateTime? Fechaedita { get; set; }
+        public bool? Isdeleted { get; set; }
+
+        [JsonIgnore]
+        public virtual ICollection<Usuariologin> Usuariologins { get; set; }
+        [JsonIgnore]
+        public virtual ICollection<Usuariomembresium> Usuariomembresia { get; set; }
+    }
+}
---
+++ Usuariologin.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+#nullable disable
+
+namespace ProyectoUSMP_GYM.Models.ModelDB
+{
+    public partial class Usuariologin
+    {
+        public int PkUsuariologin { get; set; }
+        public string Usuario { get; set; }
+        public string Passwords { get; set; }
+        public int? FkUsuario { get; set; }
+        [JsonIgnore]
+        public virtual Usuario FkUsuarioNavigation { get; set; }
+    }
+}
